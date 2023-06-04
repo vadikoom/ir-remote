cdc6bd6088735b4f04de56f729adb4e9cde9c9fb
vadim <vadim@vadims-MacBook-Pro.local>
2023-06-04 16:31:55 +0300

    firmware upload

--- firmware/controller/controller.ino
+++ firmware/controller/controller.ino
@@ -1,10 +1,71 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+
+#ifndef WIFI_SSID
+    #error "WIFI_SSID macro is not defined!"
+#endif
+
+#ifndef WIFI_PASS
+    #error "WIFI_PASS macro is not defined!"
+#endif
+
+#define UDP_PORT 4944
+
+// UDP
+WiFiUDP UDP;
+
+char buffer[] = "hello world";
+char buffer2[200];
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
+
+    delay(5000);
+
+    // Begin WiFi
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    
+    // Connecting to WiFi...
+    Serial.print("Connecting to ");
+    Serial.print(WIFI_SSID);
+    // Loop continuously while WiFi is not connected
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+        Serial.print(".");
+    }
+    
+    // Connected to WiFi
+    Serial.println();
+    Serial.print("Connected! IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Begin listening to UDP port
+    UDP.begin(UDP_PORT);
+    Serial.print("Listening on UDP port ");
+    Serial.println(UDP_PORT);
+    
 }
 
 void loop() {
-    Serial.println("Hello World!");
-    delay(1000);
+    delay(3000);
+    UDP.beginPacket("255.255.255.255", UDP_PORT);
+    UDP.write(buffer, sizeof(buffer));
+    UDP.endPacket();
+
+    int packetSize = UDP.parsePacket(); 
+    if (packetSize) {
+        Serial.print("Received packet! Size: ");
+        Serial.println(packetSize); 
+        int len = UDP.read(buffer2, 255);
+        if (len > 0) {
+            buffer2[len] = '\0';
+        }
+        Serial.print("Packet received: ");
+        Serial.println(buffer2);
+    }
+
+    Serial.println("Sent packet!");
 }
--- firmware/tester/tester.ino
+++ firmware/tester/tester.ino
@@ -1,10 +1,225 @@
-#include <Arduino.h>
+#define RAW_BUFFER_LENGTH 750
+#define RECORD_GAP_MICROS 16000
+#define DEBUG
 
-void setup() {
-    Serial.begin(9600);
+//------------------------------------------------------------------------------
+// Include the IRremote library header
+//
+#include <IRremote.h>
+
+//------------------------------------------------------------------------------
+// Tell IRremote which Arduino pin is connected to the IR Receiver (TSOP4838)
+//
+int recvPin = 11;
+IRrecv irrecv(recvPin);
+
+//+=============================================================================
+// Configure the Arduino
+//
+void  setup ( )
+{
+  Serial.begin(9600);   // Status message will be sent to PC at 9600 baud
+  irrecv.enableIRIn();  // Start the receiver
 }
 
-void loop() {
-    Serial.println("Hello World!");
-    delay(1000);
+//+=============================================================================
+// Display IR code
+//
+void  ircode (decode_results *results)
+{
+  // Panasonic has an Address
+  if (results->decode_type == PANASONIC) {
+    Serial.print(results->address, HEX);
+    Serial.print(":");
+  }
+
+  // Print Code
+  Serial.print(results->value, HEX);
+}
+
+//+=============================================================================
+// Display encoding type
+//
+void  encoding(decode_type_t x)
+{
+  switch (x) {
+    default:
+    case UNKNOWN:      Serial.print("UNKNOWN");       break ;
+    case NEC:          Serial.print("NEC");           break ;
+    case SONY:         Serial.print("SONY");          break ;
+    case RC5:          Serial.print("RC5");           break ;
+    case RC6:          Serial.print("RC6");           break ;
+//    case DISH:         Serial.print("DISH");          break ;
+    case SHARP:        Serial.print("SHARP");         break ;
+    case JVC:          Serial.print("JVC");           break ;
+ //   case SANYO:        Serial.print("SANYO");         break ;
+//    case MITSUBISHI:   Serial.print("MITSUBISHI");    break ;
+    case SAMSUNG:      Serial.print("SAMSUNG");       break ;
+    case LG:           Serial.print("LG");            break ;
+    case WHYNTER:      Serial.print("WHYNTER");       break ;
+ //   case AIWA_RC_T501: Serial.print("AIWA_RC_T501");  break ;
+    case PANASONIC:    Serial.print("PANASONIC");     break ;
+    case DENON:        Serial.print("Denon");         break ;
+  }
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpInfo (decode_results *results)
+{
+  // Check if the buffer overflowed
+  if (results->overflow) {
+    Serial.println("IR code too long. Edit IRremoteInt.h and increase RAWBUF");
+    return;
+  }
+
+  // Show Encoding standard
+  Serial.print("Encoding  : ");
+  encoding(results->decode_type);
+  Serial.println("");
+
+  // Show Code & length
+  Serial.print("Code      : ");
+  ircode(results);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpRaw (irparams_struct *results)
+{
+  // Print Raw data
+  Serial.print("Timing[");
+  Serial.print(results->rawlen-1, DEC);
+  Serial.println("]: ");
+
+  for (int i = 1;  i < results->rawlen;  i++) {
+    unsigned long  x = results->rawbuf[i] * USECPERTICK;
+    if (!(i & 1)) {  // even
+      Serial.print("-");
+      if (x < 1000)  Serial.print(" ") ;
+      if (x < 100)   Serial.print(" ") ;
+      Serial.print(x, DEC);
+    } else {  // odd
+      Serial.print("     ");
+      Serial.print("+");
+      if (x < 1000)  Serial.print(" ") ;
+      if (x < 100)   Serial.print(" ") ;
+      Serial.print(x, DEC);
+      if (i < results->rawlen-1) Serial.print(", "); //',' not needed for last one
+    }
+    if (!(i % 8))  Serial.println("");
+  }
+  Serial.println("");                    // Newline
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpCode (irparams_struct *results)
+{
+  // Start declaration
+  Serial.print("unsigned int  ");          // variable type
+  Serial.print("rawData[");                // array name
+  Serial.print(results->rawlen - 1, DEC);  // array size
+  Serial.print("] = {");                   // Start declaration
+
+  // Dump data
+  for (int i = 1;  i < results->rawlen;  i++) {
+    Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
+    if ( i < results->rawlen-1 ) Serial.print(","); // ',' not needed on last one
+    if (!(i & 1))  Serial.print(" ");
+  }
+
+  // End declaration
+  Serial.print("};");  // 
+
+  // Newline
+  Serial.println("");
+}
+
+void  dumpInfo2(IRData *results)
+{
+  // Show Encoding standard
+  Serial.print("Encoding  : ");
+  encoding(results->protocol);
+  Serial.println("");
+
+  // Show Code & length
+  Serial.print("Code      : ");
+  Serial.print(results->decodedRawData, HEX);
+  Serial.print(" (");
+  Serial.print(results->numberOfBits, DEC);
+  Serial.println(" bits)");
+
+  Serial.print("Flags     : ");
+  Serial.println(results->flags, HEX);
+
+  Serial.print("Address  : ");
+  Serial.println(results->address, HEX);
+
+
+  Serial.print("Command  : ");
+  Serial.println(results->command, HEX);
+
+  Serial.print("Bits     : ");
+  Serial.println(results->numberOfBits, DEC);
+
+  Serial.print("HeaderMarkMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.HeaderMarkMicros, DEC);
+
+  Serial.print("HeaderSpaceMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.HeaderSpaceMicros, DEC);
+
+  Serial.print("ZeroMarkMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.ZeroMarkMicros, DEC);
+
+  Serial.print("ZeroSpaceMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.ZeroSpaceMicros, DEC);
+
+  Serial.print("OneMarkMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.OneMarkMicros, DEC);
+
+  Serial.print("OneSpaceMicros   : ");
+  Serial.println(results->DistanceWidthTimingInfo.OneSpaceMicros, DEC);
+
+  Serial.print("rawlen   : ");
+  Serial.println(results->rawDataPtr->rawlen, DEC);
+
+  Serial.print("OverflowFlag   : ");
+  Serial.println(results->rawDataPtr->OverflowFlag, DEC);
+
+  Serial.print("ssiiizeee   : ");
+  Serial.println(sizeof(results->rawDataPtr->rawbuf)/sizeof(results->rawDataPtr->rawbuf[0]), DEC);
+
+  dumpRaw(results->rawDataPtr);
+  dumpCode(results->rawDataPtr);
+}
+
+//+=============================================================================
+// The repeating section of the code
+//
+void  loop ( )
+{
+
+  if (irrecv.decode()) {
+    dumpInfo2(&IrReceiver.decodedIRData);
+    Serial.println("");
+    irrecv.resume();
+  }
+
+  // decode_results  results;        // Somewhere to store the results
+
+
+  // if (irrecv.decode(&results)) {  // Grab an IR code
+  //   dumpInfo(&results);           // Output the results
+  //   dumpRaw(&results);            // Output the results in RAW format
+  //   dumpCode(&results);           // Output the results as source code
+  //   Serial.println("");           // Blank line between entries
+  //   irrecv.resume();              // Prepare for the next value
+  // }
 }
