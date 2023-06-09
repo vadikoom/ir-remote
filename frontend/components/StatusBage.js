f073c3af1fc8cbdeac60b9e48fee13d373fc705f
Vadim <vadik.ya@gmail.com>
2023-06-09 16:48:46 +0300

    minor ui change

--- frontend/components/StatusBage.js
+++ frontend/components/StatusBage.js
@@ -2,15 +2,15 @@ import { api } from "../lib/api";
 import { useEffect, useState } from "react";
 
 export const StatusBage = () => {
-  const [isOnline, setIsOnline] = useState(false);
+  const [{ isOnline, error }, setIsOnline] = useState({});
   useEffect(() => {
     async function getStatus() {
-        try {
-            let { online } = await api.getStatus();
-            setIsOnline(online);
-        } catch (error) {
-            setIsOnline(false);
-        }
+      try {
+        let { online } = await api.getStatus();
+        setIsOnline({ isOnline: online });
+      } catch (error) {
+        setIsOnline({ isOnline: false, error });
+      }
     }
     getStatus();
     const h = setInterval(getStatus, 3000);
@@ -18,6 +18,22 @@ export const StatusBage = () => {
       clearInterval(h);
     };
   }, []);
+
+  if (error) {
+    return (
+      <span style={{ color: "darkred", fontWeight: "bold" }}>
+        {(error && error.message) || error}
+      </span>
+    );
+  }
+
+  if (isOnline === undefined) {
+    return (
+      <span style={{ color: "darkgray", fontWeight: "bold" }}>
+        checking status...
+      </span>
+    );
+  }
 
   if (isOnline) {
     return (
